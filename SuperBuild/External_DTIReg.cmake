58049f7cc4e45653ce65b14cb3bd6efa25320a60
fbudin69500 <fbudin@unc.edu>
2014-09-09 10:48:19 -0400

    ENH: Update DTI-Reg version

--- SuperBuild/External_DTIReg.cmake
+++ SuperBuild/External_DTIReg.cmake
@@ -69,7 +69,7 @@ if(NOT ( DEFINED "USE_SYSTEM_${extProjName}" AND "${USE_SYSTEM_${extProjName}}" 
 
   ### --- End Project specific additions
   set(${proj}_REPOSITORY https://www.nitrc.org/svn/dtireg/trunk)
-  set(${proj}_REVISION -r "74")
+  set(${proj}_REVISION -r "75")
 
   ExternalProject_Add(${proj}
     SVN_REPOSITORY ${${proj}_REPOSITORY}
