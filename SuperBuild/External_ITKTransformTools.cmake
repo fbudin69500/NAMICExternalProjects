8265c5b8c78b9855da776fba331f48b3ed5bb033
fbudin69500 <fbudin@unc.edu>
2014-09-11 17:29:04 -0400

    ENH: Update version of ITKTransformTools

--- SuperBuild/External_ITKTransformTools.cmake
+++ SuperBuild/External_ITKTransformTools.cmake
@@ -61,7 +61,7 @@ if(NOT ( DEFINED "USE_SYSTEM_${extProjName}" AND "${USE_SYSTEM_${extProjName}}" 
 
   ### --- End Project specific additions
   set( ${proj}_REPOSITORY ${git_protocol}://github.com/fbudin69500/ITKTransformTools.git )
-  set( ${proj}_GIT_TAG 2af799cbe1c1b0235416f38e9dffb14a6af078d7 )
+  set( ${proj}_GIT_TAG 44135e503ef3c6285f04cedb80433ffa8c7c376c )
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
     GIT_TAG ${${proj}_GIT_TAG}
