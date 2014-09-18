f443cd12696ce5a26e7888d52408f373f39db4de
fbudin69500 <fbudin@unc.edu>
2014-09-18 16:26:01 -0400

    ENH: Update NeosegPipeline to v1.0.6

--- SuperBuild/External_NeosegPipeline.cmake
+++ SuperBuild/External_NeosegPipeline.cmake
@@ -83,7 +83,7 @@ if(NOT ( DEFINED "USE_SYSTEM_${extProjName}" AND "${USE_SYSTEM_${extProjName}}" 
 
   ### --- End Project specific additions
   set( ${proj}_REPOSITORY ${git_protocol}://github.com/NIRALUser/NeosegPipeline.git )
-  set( ${proj}_GIT_TAG 41d9ec2bd1a01753255c5acf16de3802dea8d507 )
+  set( ${proj}_GIT_TAG 19611b1c4ed00b517e4846dc3455c6f82847729e )
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
     GIT_TAG ${${proj}_GIT_TAG}
